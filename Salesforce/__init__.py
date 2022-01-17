e8380c24d9406b4d29714eb40e54b1172970c07e
madong <viaimperiale@gmail.com>
2022-01-17 18:54:49 +0100

    Added Salesforce module

---
+++ Salesforce/__init__.py
@@ -0,0 +1,10 @@
+import json
+import os
+from simple_salesforce import Salesforce
+
+secrets = os.environ if os.environ.get('is_production') else json.load(open(os.path.join("..", "secrets")))
+
+username = secrets["sf_username"]
+password = secrets["sf_password"]
+security_token = secrets["sf_security_token"]
+sf = Salesforce(username=username, password=password, security_token=security_token)
