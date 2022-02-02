61cdf6328c65200a36529e146da8282796978b83
Marco <viaimperiale@gmail.com>
2022-02-02 23:38:00 +0100

    Echo on ISW bot

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask, request
 from telegram import Bot
 from telegram.ext import Updater
@@ -17,10 +19,16 @@ def index():
     return "Hello World!"
 
 
-@app.route(f"/taiga", methods=["POST"])
-def callback():
-    print(request.data)
-    print(request.headers)
+@app.route(f"/taiga/<chat_id>", methods=["POST"])
+def callback(chat_id):
+    data = json.loads(request.data)
+    print(f'Progetto: {data["data"]["project"]["name"]}')
+    try:
+        isw_bot.send_message(chat_id=chat_id, text=f'Progetto: {data["data"]["project"]["name"]}')
+    except Exception as e:
+        print(e)
+
+    return 200
 
 
 print("Bot started!")
